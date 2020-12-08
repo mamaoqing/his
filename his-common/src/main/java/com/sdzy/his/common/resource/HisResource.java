345dd5ff682eb67b08d619c48390412c820bcb74
mamaoqing <luyuna1121@163.com>
2020-12-08 17:36:41 +0800

    添加拦截器

---
+++ his-common/src/main/java/com/sdzy/his/common/resource/HisResource.java
@@ -0,0 +1,12 @@
+package com.sdzy.his.common.resource;
+
+/**
+ * @author mq
+ * @description: TODO
+ * @title: HisResource
+ * @projectName his
+ * @date 2020/12/817:28
+ */
+public class HisResource {
+    public static final Integer SUCCESS_CODE = 200;
+}
--- his-web/src/main/java/com/sdzy/his/web/interceptor/LoginInterceptor.java
+++ his-web/src/main/java/com/sdzy/his/web/interceptor/LoginInterceptor.java
@@ -1,6 +1,9 @@
 package com.sdzy.his.web.interceptor;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sdzy.his.common.entity.Result;
+import com.sdzy.his.common.enums.HisExceptionEnum;
+import com.sdzy.his.common.resource.HisResource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 /**
  * @author mq
@@ -31,21 +35,33 @@ public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         String token = String.valueOf(request.getHeader("Authentication-Token"));
         Result result = restTemplate.getForObject(serverUrl + "/login/getUser?token={token}", Result.class,token);
-        log.info("用户登录信息：{}",result.getData());
-        if(result.getCode() == 200){
+        assert result != null;
+        if(HisResource.SUCCESS_CODE.equals(result.getCode())){
             return true;
         }
         log.error("用户登录失效，请重新登录！");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType( "application/json; charset=utf-8");
+        PrintWriter pw = null;
+        try{
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("code", HisExceptionEnum.LOGIN_TIME_OUT.getCode());
+            jsonObject.put("message",HisExceptionEnum.LOGIN_TIME_OUT.getMsg());
+            pw = response.getWriter();
+            pw.append(jsonObject.toJSONString());
+        }catch (Exception ignored){
+            ignored.printStackTrace();
+        }
         return false;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
 
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
     }
 }
